7528196c068992396a4f4167212e993372e69cd6
TheoWestbrooke <theo@westbrooke.com>
2026-05-05 10:56:59 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,9 @@
+const CACHE = 'hc-debt-model-v5';
+const ASSETS = ['./','./index.html','./style.css','./model.js','./app.js','./jszip.min.js','./manifest.webmanifest','./assets/HC Debt Only Model.xlsx'];
+self.addEventListener('install', event => { event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting())); });
+self.addEventListener('activate', event => { event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim())); });
+self.addEventListener('fetch', event => {
+  event.respondWith(fetch(event.request).then(response => {
+    const copy=response.clone(); caches.open(CACHE).then(cache => cache.put(event.request, copy)).catch(()=>{}); return response;
+  }).catch(() => caches.match(event.request)));
+});
